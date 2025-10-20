11ab7afd9049ef118bc6aa34e24dd7069cad092f
Nikola Domagoj <nikola.domagoj@gmail.com>
2025-10-20 23:18:08 +0200

    initial commit

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,27 @@
+const notchPage = require('../pageobjects/notch.page');
+
+const loginURL = 'https://wearenotch.com/qa_task/'; 
+
+describe('Notch qa task', () => {
+
+    let firstName;
+    let lastName;
+    let email;
+    let phoneNumber;
+
+    before('Daj vrijednost ovim iznad varijablama', async () => {
+        firstName = 'pero';
+        lastName = 'peric';
+        email = 'pero.peric@gmail.com'
+        phoneNumber = '0973205627'
+        await browser.url(loginURL);
+    });
+    it('Popunjavanje polja sa vrijednostima iznad', async () => {
+        
+        await notchPage.firstName.setValue(firstName);
+        await notchPage.lastName.setValue(lastName);
+        await notchPage.email.setValue(email);
+        await notchPage.phoneNumber.setValue(phoneNumber);
+        
+    })
+})
