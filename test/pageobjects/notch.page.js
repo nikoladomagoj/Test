79d8f58e12ad3d030d94be139a517b6adc3f921e
Zvonimir Knezevic <zvonimir.knezevic@beeline.com>
2025-10-27 01:42:56 +0100

    zvone_update_comments

--- test/pageobjects/notch.page.js
+++ test/pageobjects/notch.page.js
@@ -15,13 +15,13 @@ class teste2e {
         return $('input[id="input_7_8"]');
     }
     get hdyhau() {
-        return $('select[id="input_7_9"]');
+        return $('div[id="field_7_9"]');
     }
     get company() {
-        return $('input[id="input_7_11"]');
+        return $('div[id="field_7_11"]');
     }
     get budget() {
-        return $('select[id="input_7_12"]');
+        return $('div[id="field_7_12"]');
     }
     get privacyPolicy() {
         return $('input[id="input_7_16_1"]');
@@ -30,8 +30,33 @@ class teste2e {
         return $('button[id="gform_submit_button_7"]');
     }
     get emailError() {
-    return $('div[id="validation_message_7_17"]');
+        return $('div[id="validation_message_7_17"]');
     }
+    get checkbox() {
+        return $('input[id="input_7_16_1"]');
+    }
+    get sendMessage() {
+        return $('button[data-text="Send message"]');
+    }
+
+    // ovo je funkcija, async znači asynkrono, to jest stalno se poziva dok se ne zovne
+    // imaš async funckije i sync funkcije, kod testiranja se stalno koriste async
+    // <li class="active-result" data-option-array-index="2">Google</li> ovo je element od google da kliknemo
+    // mi smo ispod dolje napraviti da mi funckiju koja uđe u ove elemente i traži tekst, teks smo koristili u testu
+    //istu stvar smo napravili za budget
+
+    async selectHowDidYouHear(option) {
+        const optionToSelect = $(`//li[contains(@class,'active-result') and text()='${option}']`);
+        await optionToSelect.waitForClickable();
+        await optionToSelect.click();
+    }
+
+    async selectBudgetOption(optionText) {
+        const option = $(`//li[contains(@class,"active-result") and normalize-space()="${optionText}"]`);
+        await option.waitForDisplayed({ timeout: 4000});
+        await option.click();
+    }
+
 }
 
 module.exports = new teste2e();
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -17,16 +17,14 @@ describe('Notch qa task', () => {
         phoneNumber = '0973205627';
         company = "Apple"
         await browser.url(loginURL);
-/*
+
         const cookieAccept = await $('button.cky-btn-accept');
         if (await cookieAccept.isDisplayed()) {
         await cookieAccept.click();
-        
-    }*/
+
     });
-    /*
+
     it('Popunjavanje polja bez email-a', async () => {
-        //await notchPage.cookie.click();
         await notchPage.firstName.setValue(firstName);
         await notchPage.lastName.setValue(lastName);
         //await notchPage.email.setValue(email);
@@ -43,24 +41,23 @@ describe('Notch qa task', () => {
         await notchPage.phoneNumber.setValue(phoneNumber);
 
     });
-*/
-    it('Popunjavanje sa više polja', async () => {
-        await notchPage.cookie.waitForDisplayed({timeout:1000});
-        await notchPage.cookie.click();
 
+    it('Popunjavanje sa više polja', async () => {
         await notchPage.firstName.setValue(firstName);
         await notchPage.lastName.setValue(lastName);
         await notchPage.email.setValue(email);
         await notchPage.phoneNumber.setValue(phoneNumber);
+        await notchPage.hdyhau.click(); //klikni na How did you hear about us da dobijemo padajući izbornik
+        await notchPage.selectHowDidYouHear("Google"); // nađi mi lement koji sandrži u sebi riječ Google
+        await notchPage.company.click(); //klikni na company
+        await browser.keys("Test"); //pošalji neka slova u ovom slučaju popunit će se kao riječ Test
+        await notchPage.budget.click(); //klikni na budget
+        await notchPage.selectBudgetOption("Up to €50.000"); // nađi element koji u sebi sadrži ovo (pogledaj pageobjects kak osam definirao)
+        await notchPage.checkbox.click(); //klikni na checkbox
+        await notchPage.sendMessage.click(); //klikni na sendmessage
+        });
+ //elemente nisi dobro našao, krivo si gledao to te je zbunilo
+     });
 
-        
-        //await notchPage.hdyhau.scrollIntoView();
-        //await notchPage.hdyhau.waitForDisplayed({ timeout: 5000 });
-        await notchPage.hdyhau.selectByVisibleText("Facebook");
-
-        await notchPage.company.setValue(company);
-
-        await notchPage.budget.selectByVisibleText('Up to €50.000');
-
-    });
+ // kad sve dobro proučiš možeš izbrisati sve ove komentare ovdje i u pageobjects
 });
